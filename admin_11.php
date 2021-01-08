8ea6a4aff75e49752076ca594acffd8d16f4e58f
gizemmuratoglu <58625642+gizemmuratoglu@users.noreply.github.com>
2021-01-08 22:11:56 +0300

    Update admin_11.php
    - admin can enter current month's due
    -change table and add form in the table.

--- admin_11.php
+++ admin_11.php
@@ -53,59 +53,59 @@ include 'baglan.php';
 			float: left;
 
 		}
-			.navigation ul{
-				position: absolute;
-				top: 0;
-				left: 0;
-				width: 100%;
+		.navigation ul{
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
 
-			}
-			.navigation ul li{
-				position: relative;
-				width: 100%;
-				list-style: none;
+		}
+		.navigation ul li{
+			position: relative;
+			width: 100%;
+			list-style: none;
 
-			}
-			.navigation ul li a{
-				position: relative;
-				display: block;
-				width: 100%;
-				display: flex;
-				text-decoration: none;
-				color: white;
-			}
+		}
+		.navigation ul li a{
+			position: relative;
+			display: block;
+			width: 100%;
+			display: flex;
+			text-decoration: none;
+			color: white;
+		}
 
-			.navigation ul li a .icon .fa{
-				font-size: 24px;
-			}
-			.navigation ul li a .title{
-				padding-left: 0 !important;
-				font-size: 15.5px;
-				color: white;
-				text-decoration-line: underline;
-				position: relative;
-				display: block;
-				padding: 0 20px;
-				line-height: 60px;
-				text-align: start;
-				white-space: nowrap;
-			}
-			.navigation ul li a .title:hover{
-				color: brown;
-				font-size: 16px;
+		.navigation ul li a .icon .fa{
+			font-size: 24px;
+		}
+		.navigation ul li a .title{
+			padding-left: 0 !important;
+			font-size: 15.5px;
+			color: white;
+			text-decoration-line: underline;
+			position: relative;
+			display: block;
+			padding: 0 20px;
+			line-height: 60px;
+			text-align: start;
+			white-space: nowrap;
+		}
+		.navigation ul li a .title:hover{
+			color: brown;
+			font-size: 16px;
 
-			}
-			.navigation ul li a .baslık{
-				font-size: 24px;
-				color: white;
-				text-decoration-line: underline;
-				position: relative;
-				display: block;
-				padding: 0 20px;
-				line-height: 60px;
-				text-align: start;
-				white-space: nowrap;
-			}
+		}
+		.navigation ul li a .baslık{
+			font-size: 24px;
+			color: white;
+			text-decoration-line: underline;
+			position: relative;
+			display: block;
+			padding: 0 20px;
+			line-height: 60px;
+			text-align: start;
+			white-space: nowrap;
+		}
 
 	</style>
 	
@@ -114,55 +114,54 @@ include 'baglan.php';
 <body>
 	<div class="wlc" style="text-align: center; font-size: 25px; color: white; "> <b>HOST DETAILS</b>   </div>
 	<div class="con">
-			<div class="navigation">
-				<ul>
-					<li>
-						<a >
+		<div class="navigation">
+			<ul>
+				<li>
+					<a >
 
-							<span class="baslık" style="text-decoration-line: none;"><b>DASHBOARD</b> </span>
-						</a>
-					</li>
-					<<li>
-						<a href="admin_11.php" >
+						<span class="baslık" style="text-decoration-line: none;"><b>DASHBOARD</b> </span>
+					</a>
+				</li>
+				<<li>
+					<a href="admin_11.php" >
 
-							<span class="title" > HOST DETAILS&EDIT</span>
-						</a>
-					</li>
-					<li>
-						<a href="newPage.php">
-							<span class="title">ADD NEW HOST</span>
-						</a>
-					</li>
-					<li>
-						<a href="currentdue.php">
-							<span class="title">EDİT DUE&DETAILS</span>
-						</a>
-					</li>
-					<li>
-						<a href="report.php">
-							<span class="title">CHECK PROBLEM&REQUEST</span>
-						</a>
-					</li>
-					<li>
-						<a href="see.php">
-							<span class="title">EDIT INCOME-EXPENSE TABLE</span>
-						</a>
-					</li>
-					<li>
-						<a href="admin.php">
-							<span class="title">LOG OUT</span>
-						</a>
-					</li>
-				</ul>
-			</div>
+						<span class="title" > HOST DETAILS&EDIT</span>
+					</a>
+				</li>
+				<li>
+					<a href="newPage.php">
+						<span class="title">ADD NEW HOST</span>
+					</a>
+				</li>
+				<li>
+					<a href="currentdue.php">
+						<span class="title">EDİT DUE&DETAILS</span>
+					</a>
+				</li>
+				<li>
+					<a href="report.php">
+						<span class="title">CHECK PROBLEM&REQUEST</span>
+					</a>
+				</li>
+				<li>
+					<a href="see.php">
+						<span class="title">EDIT INCOME-EXPENSE TABLE</span>
+					</a>
+				</li>
+				<li>
+					<a href="admin.php">
+						<span class="title">LOG OUT</span>
+					</a>
+				</li>
+			</ul>
 		</div>
-
+	</div>
 
 	<br><br><br><br><br>
 	<div class="container">
 		<table style=" width: 100%;" border="1px" class="table table-secondary table-striped table-bordered table-hover">
 			<tr>
-				<th>NUMBER</th>
+				
 				<th>BLOCK NAME</th>
 				<th>NAME</th>
 				<th>SURNAME</th>
@@ -170,24 +169,25 @@ include 'baglan.php';
 				<th>TELEPHONE1</th>
 				<th>TELEPHONE2</th>
 				<th>MOVE IN</th>
-				<th>MOVE OUT</th>
 				<th>OPERATION</th>
+				<th>ADD NEW MONTH DUE</th>
+
 				
 
 			</tr>
 
 
 			<?php
-			$bilgilerisor=$db->prepare("SELECT * FROM bilgiler ORDER BY move_out ");
+			$bilgilerisor=$db->prepare("SELECT * FROM bilgiler ORDER BY blockname ");
 			$bilgilerisor->execute();
 			$sayı=0;
-			while ($bilgileriçek=$bilgilerisor->FETCH(PDO::FETCH_ASSOC)) { $sayı++; ?>
+			while ($bilgileriçek=$bilgilerisor->FETCH(PDO::FETCH_ASSOC)) {  ?>
 
 
 
 
 				<tr>
-					<td><?php echo $sayı;  ?></td>
+					
 					<td><?php echo $bilgileriçek['blockname'] ; ?></td>
 					<td><?php echo $bilgileriçek['name'] ; ?></td>
 					<td><?php echo $bilgileriçek['surname'] ; ?></td>
@@ -195,14 +195,40 @@ include 'baglan.php';
 					<td><?php echo $bilgileriçek['telephone_num1'] ; ?></td>
 					<td><?php echo $bilgileriçek['telephone_num2'] ; ?></td>
 					<td><?php echo $bilgileriçek['move_in'] ; ?></td>
-					<td><?php echo $bilgileriçek['move_out'] ; ?></td>
-					<td align="center"> <a href="edit.php?id=<?php echo $bilgileriçek['id']?>"> <button class="btn btn-dark" >EDIT HOST</button></td></a>
+					<td align="center"> 
+						<a href="edit.php?id=<?php echo $bilgileriçek['id']?>"> <button class="btn btn-dark" >EDIT HOST</button> </td></a>
+					
+					<td align="center"> 
+						<form action="insert.php?hostid=<?php echo $bilgileriçek['id']?>&start=ok" method="POST"> 
+							<input type="text" name="amount" required="">
+
+						 <button class="btn btn-danger" >START NEW MONTH</button> </td></form>
 					
 				</tr>
 			<?php } ?>
+			<?php
+
+			if($_GET['durum']=="var"){ ?>
+				<div class="alert alert-danger ">
+					<a href="admin_11.php" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+					<strong>Warning!</strong> This period already created .
+				</div>
+			<?php  } ?>
+			<?php 
+
+           if($_GET['sonuc']=="ok"){ ?>
+				<div class="alert alert-success ">
+					<a href="admin_11.php" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+					<strong>Success!</strong> This period created .
+				</div>
+			<?php  } 
+			 ?>
+
+			
+			
 		</table>
 
 	</div>
-
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 </html>
