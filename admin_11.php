abb4a0bc0c670508ffe7f2b56b0b8168e04a5d2d
gizemmuratoglu <58625642+gizemmuratoglu@users.noreply.github.com>
2021-01-27 22:22:55 +0300

    Update admin_11.php

--- admin_11.php
+++ admin_11.php
@@ -6,35 +6,30 @@ include 'baglan.php';
 <html>
 <head>
 	<title>HostDetail/ERENHOUSES</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	
 	<style >
 		@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300&display=swap');
 		body{
-			
-			/*background-color: #bababa !important;*/
 			background-size: 100% 758px;
-			background-image: url('img4.jpg'); 
+			background-image: url('img4.jpg');
+			/*background-color:#bababa !important;*/
 		}
-		*{
-
-			margin: 0;
-			padding:0;
-			box-sizing: border-box;
-			font-family:'Poppins', sans-serif;
-		}
-		.container{
-			/*float: right;
-
-			*/
+		.cont{
 			
-			padding-left: 140px !important;
-			text-align: center !important;
-			margin-left: 150px !important;
+			/*background-color: grey;*/
+			padding-top: 18px;
+			width: 79%;
+			
+			margin-left: 282px;
 
-			padding-right: 0px !important;
-			width: 84% !important;
-			/*background-color: white !important;*/
+			
 		}
+		.kolon{
+			display: inline-block;
+			border:1px  padding:10px; margin:10px; 
+			text-align: center;
+		}
+
 		.wlc{
 			padding-top: 20px;
 			float: left;
@@ -52,114 +47,112 @@ include 'baglan.php';
 			overflow: hidden;
 			float: left;
 
-		}
-		.navigation ul{
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
+			}.navigation ul{
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
 
-		}
-		.navigation ul li{
-			position: relative;
-			width: 100%;
-			list-style: none;
+			}
+			.navigation ul li{
+				position: relative;
+				width: 100%;
+				list-style: none;
 
-		}
-		.navigation ul li a{
-			position: relative;
-			display: block;
-			width: 100%;
-			display: flex;
-			text-decoration: none;
-			color: white;
-		}
+			}
+			.navigation ul li a{
+				position: relative;
+				display: block;
+				width: 100%;
+				display: flex;
+				text-decoration: none;
+				color: white;
+			}
 
-		.navigation ul li a .icon .fa{
-			font-size: 24px;
-		}
-		.navigation ul li a .title{
-			padding-left: 0 !important;
-			font-size: 15.5px;
-			color: white;
-			text-decoration-line: underline;
-			position: relative;
-			display: block;
-			padding: 0 20px;
-			line-height: 60px;
-			text-align: start;
-			white-space: nowrap;
-		}
-		.navigation ul li a .title:hover{
-			color: brown;
-			font-size: 16px;
+			.navigation ul li a .icon .fa{
+				font-size: 24px;
+			}
+			.navigation ul li a .title{
+				padding-left: 0 !important;
+				font-size: 15.5px;
+				color: white;
+				text-decoration-line: underline;
+				position: relative;
+				display: block;
+				padding: 0 20px;
+				line-height: 60px;
+				text-align: start;
+				white-space: nowrap;
+			}
+			.navigation ul li a .title:hover{
+				color: brown;
+				font-size: 16px;
 
-		}
-		.navigation ul li a .baslık{
-			font-size: 24px;
-			color: white;
-			text-decoration-line: underline;
-			position: relative;
-			display: block;
-			padding: 0 20px;
-			line-height: 60px;
-			text-align: start;
-			white-space: nowrap;
-		}
+			}
+			.navigation ul li a .baslık{
+				font-size: 24px;
+				color: white;
+				text-decoration-line: underline;
+				position: relative;
+				display: block;
+				padding: 0 20px;
+				line-height: 60px;
+				text-align: start;
+				white-space: nowrap;
+			}
 
 	</style>
 	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
-	<div class="wlc" style="text-align: center; font-size: 25px; color: white; "> <b>HOST DETAILS</b>   </div>
-	<div class="con">
-		<div class="navigation">
-			<ul>
-				<li>
-					<a >
+	<div class="wlc" style="text-align: center; font-size: 25px; color: white; "> <b>EDIT DUE&DETAILS</b> </div> 
+		<div class="con">
+			<div class="navigation">
+				<ul>
+					<li>
+						<a >
 
-						<span class="baslık" style="text-decoration-line: none;"><b>DASHBOARD</b> </span>
-					</a>
-				</li>
-				<<li>
-					<a href="admin_11.php" >
+							<span class="baslık" style="text-decoration-line: none;"><b>DASHBOARD</b> </span>
+						</a>
+					</li>
+					<<li>
+						<a href="admin_11.php" >
 
-						<span class="title" > HOST DETAILS&EDIT</span>
-					</a>
-				</li>
-				<li>
-					<a href="newPage.php">
-						<span class="title">ADD NEW HOST</span>
-					</a>
-				</li>
-				<li>
-					<a href="currentdue.php">
-						<span class="title">EDİT DUE&DETAILS</span>
-					</a>
-				</li>
-				<li>
-					<a href="report.php">
-						<span class="title">CHECK PROBLEM&REQUEST</span>
-					</a>
-				</li>
-				<li>
-					<a href="see.php">
-						<span class="title">EDIT INCOME-EXPENSE TABLE</span>
-					</a>
-				</li>
-				<li>
-					<a href="admin.php">
-						<span class="title">LOG OUT</span>
-					</a>
-				</li>
-			</ul>
+							<span class="title" > HOST DETAILS&EDIT</span>
+						</a>
+					</li>
+					<li>
+						<a href="newPage.php">
+							<span class="title">ADD NEW HOST</span>
+						</a>
+					</li>
+					<li>
+						<a href="currentdue.php">
+							<span class="title">EDİT DUE&DETAILS</span>
+						</a>
+					</li>
+					<li>
+						<a href="report.php">
+							<span class="title">CHECK PROBLEM&REQUEST</span>
+						</a>
+					</li>
+					<li>
+						<a href="see.php">
+							<span class="title">EDIT INCOME-EXPENSE TABLE</span>
+						</a>
+					</li>
+					<li>
+						<a href="admin.php">
+							<span class="title">LOG OUT</span>
+						</a>
+					</li>
+				</ul>
+			</div>
 		</div>
-	</div>
-
 	<br><br><br><br><br>
-	<div class="container">
-		<table style=" width: 100%;" border="1px" class="table table-secondary table-striped table-bordered table-hover">
+	<div class="cont">
+		<table style="  width: 100%;" border="1px" class="table table-secondary table-striped table-bordered table-hover">
 			<tr>
 				
 				<th>BLOCK NAME</th>
@@ -178,7 +171,7 @@ include 'baglan.php';
 
 
 			<?php
-			$bilgilerisor=$db->prepare("SELECT * FROM bilgiler ORDER BY blockname ");
+			$bilgilerisor=$db->prepare("SELECT * FROM bilgiler ORDER BY move_in ");
 			$bilgilerisor->execute();
 			$sayı=0;
 			while ($bilgileriçek=$bilgilerisor->FETCH(PDO::FETCH_ASSOC)) {  ?>
@@ -223,9 +216,6 @@ include 'baglan.php';
 				</div>
 			<?php  } 
 			 ?>
-
-			
-			
 		</table>
 
 	</div>
