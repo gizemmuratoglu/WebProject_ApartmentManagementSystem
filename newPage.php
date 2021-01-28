5e0ed92f7c27b8cb1c01474bf318bd4d7e0fb6d2
gizemmuratoglu <58625642+gizemmuratoglu@users.noreply.github.com>
2021-01-28 22:14:54 +0300

    Update newPage.php
    change ui

--- newPage.php
+++ newPage.php
@@ -6,15 +6,29 @@
 	<style type="text/css">
 		@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300&display=swap');
 
-		.container{
-			padding-top: 11px;
-			
+		body{
+			background-size: 100% 758px;
+			/*background-image: url('img4.jpg');*/
 
+			/*background-color:#bababa !important;*/
 		}
-		.bolum{
-			width: 50%;
-			padding-left: 25px;
+		.cont{
+			
+			/*background-color: grey;*/
+			padding-top: 18px;
+			width: 70%;
+			
+			margin-left: 360px;
+
+
+			
 		}
+		.kolon{
+			display: inline-block;
+			border:1px  padding:10px; margin:10px; 
+			text-align: center;
+		}
+
 		.wlc{
 			padding-top: 20px;
 			float: left;
@@ -23,93 +37,216 @@
 			background-color: #292626;
 			font-family:'Poppins', sans-serif;
 		}
-	</style>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+		.navigation{
+			position: fixed;
+			width: 250px;
+			height: 100%;
+			background-color: #292626;
+			transition: 0.5s;
+			overflow: hidden;
+			float: left;
 
-</head>
+			}.navigation ul{
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
 
-<body>
-	<div class="wlc" style="text-align: center; font-size: 25px; color: white; "> <b>ADD NEW HOST</b>   </div>
-	<br><br><br><br>
+			}
+			.navigation ul li{
+				position: relative;
+				width: 100%;
+				list-style: none;
+
+			}
+			.navigation ul li a{
+				position: relative;
+				display: block;
+				width: 100%;
+				display: flex;
+				text-decoration: none;
+				color: white;
+			}
+
+			.navigation ul li a .icon .fa{
+				font-size: 24px;
+			}
+			.navigation ul li a .title{
+				padding-left: 0 !important;
+				font-size: 15.5px;
+				color: white;
+				text-decoration-line: underline;
+				position: relative;
+				display: block;
+				padding: 0 20px;
+				line-height: 60px;
+				text-align: start;
+				white-space: nowrap;
+			}
+			.navigation ul li a .title:hover{
+				color: brown;
+				font-size: 16px;
+
+			}
+			.navigation ul li a .baslık{
+				font-size: 24px;
+				color: white;
+				text-decoration-line: underline;
+				position: relative;
+				display: block;
+				padding: 0 20px;
+				line-height: 60px;
+				text-align: start;
+				white-space: nowrap;
+			}
 
 
-	<div class="container">
-		<div class="bolum">
-			<form action="insert.php" method="POST"> 
-				<?php
+			.container{
+				padding-top: 11px;
 
-				if (isset($_GET['durum'])) {
+
+			}
+			.bolum{
+				width: 50%;
+				padding-left: 25px;
+			}
+			.wlc{
+				padding-top: 20px;
+				float: left;
+				width: 100%;
+				height: 71px;
+				background-color: #292626;
+				font-family:'Poppins', sans-serif;
+			}
+		</style>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+	</head>
+
+	<body>
+
+		<div class="wlc" style="text-align: center; font-size: 25px; color: white; "> <b>ADD NEW HOST</b>   </div>
+		<div class="con">
+			<div class="navigation">
+				<ul>
+					<li>
+						<a >
+
+							<span class="baslık" style="text-decoration-line: none;"><b>DASHBOARD</b> </span>
+						</a>
+					</li>
+					<<li>
+						<a href="admin_11.php" >
+
+							<span class="title" > HOST DETAILS&EDIT</span>
+						</a>
+					</li>
+					<li>
+						<a href="newPage.php">
+							<span class="title">ADD NEW HOST</span>
+						</a>
+					</li>
+					<li>
+						<a href="currentdue.php">
+							<span class="title">EDIT DUE&DETAILS</span>
+						</a>
+					</li>
+					<li>
+						<a href="report.php">
+							<span class="title">CHECK PROBLEM&REQUEST</span>
+						</a>
+					</li>
+					<li>
+						<a href="see.php">
+							<span class="title">EDIT INCOME-EXPENSE TABLE</span>
+						</a>
+					</li>
+					<li>
+						<a href="admin.php">
+							<span class="title">LOG OUT</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<br><br><br><br>
+
+
+		<div class="cont">
+			<div >
+				<form action="insert.php" method="POST"> 
+					<?php
+
+					if (isset($_GET['durum'])) {
 				# code...
 
-					if($_GET['durum']=="NO"){
+						if($_GET['durum']=="NO"){
 
 
-						?>
-						<div class="alert alert-danger"> BLOCKNAME ARE NOT USABLE.</div>
-						<?php
+							?>
+							<div class="alert alert-danger"> BLOCKNAME ARE NOT USABLE.</div>
+							<?php
+						}
+						else{ ?>
+							<div class="alert alert-success"> NEW HOST HAS BEEN ADDED</div>
+							<?php 
+						}
+
 					}
-					else{ ?>
-						<div class="alert alert-success"> NEW HOST HAS BEEN ADDED</div>
-						<?php 
-					}
 
-				}
+					?>
+					<div class="mb-3" >
 
-				?>
-				<div class="mb-3" >
+						<label >NAME</label>
+						<input type="text" name="name" required="" class="form-control" autocomplete="off"  placeholder="Enter your name...">
 
-					<label >NAME</label>
-					<input type="text" name="name" required="" class="form-control" autocomplete="off"  placeholder="Enter your name...">
+					</div>
+					<div class="mb-3">
+						<label for="exampleInputPassword1"  class="form-label">SURNAME</label>
+						<input type="text" required="" name="surname" autocomplete="off" class="form-control" placeholder="Enter your surname...">
+					</div>
+					<div class="mb-3">
+						<label >HOUSEMATE(S)</label>
+						<input type="text" name="housemate" class="form-control" autocomplete="off" >
 
-				</div>
-				<div class="mb-3">
-					<label for="exampleInputPassword1"  class="form-label">SURNAME</label>
-					<input type="text" required="" name="surname" autocomplete="off" class="form-control" placeholder="Enter your surname...">
-				</div>
-				<div class="mb-3">
-					<label >HOUSEMATE(S)</label>
-					<input type="text" name="housemate" class="form-control" autocomplete="off" >
+					</div>
+					<div class="mb-3">
+						<label >TELEPHONE1</label>
+						<input type="text" name="telephone_num1" required="" class="form-control" placeholder="Enter your telephone number..">
+					</div>
+					<div class="mb-3">
+						<label >TELEPHONE2</label>
+						<input type="text" name="telephone_num2" class="form-control">
+					</div>
 
-				</div>
-				<div class="mb-3">
-					<label >TELEPHONE1</label>
-					<input type="text" name="telephone_num1" required="" class="form-control" placeholder="Enter your telephone number..">
-				</div>
-				<div class="mb-3">
-					<label >TELEPHONE2</label>
-					<input type="text" name="telephone_num2" class="form-control">
-				</div>
-
-				<div class="mb-3">
-					<label >PASSWORD</label>
-					<input type="password" name="password"  class="form-control" required="" placeholder="Enter your password...">
-				</div>
-				<div class="mb-3">
-					<label >BLOCKNAME</label>
-					<select name="blockname" id="block">
-						<option value="block1"> BLOCK 1</option>
-						<option value="block2"> BLOCK 2</option> 
-						<option value="block2"> BLOCK 3</option> 
-						<option value="block2"> BLOCK 4</option> 
-						<option  value="block2">BLOCK 5</option>
-					</select>
-				</div>
-				<br>
+					<div class="mb-3">
+						<label >PASSWORD</label>
+						<input type="password" name="password"  class="form-control" required="" placeholder="Enter your password...">
+					</div>
+					<div class="mb-3">
+						<label >BLOCKNAME</label>
+						<select name="blockname" id="block">
+							<option value="block1"> BLOCK 1</option>
+							<option value="block2"> BLOCK 2</option> 
+							<option value="block3"> BLOCK 3</option> 
+							<option value="block4"> BLOCK 4</option> 
+							<option  value="block5">BLOCK 5</option>
+						</select>
+					</div>
+					<br>
 
 
 
-				<button type="submit" class="btn btn-primary" name="insertislemi">CREATE ACCOUNT</button> <br> <br>
-				<div style="padding-left:0;">
-					<a href="admin_1.php">BACK TO ADMIN PANEL</a> <br><br><br>
-				</div>
+					<button type="submit" class="btn btn-primary" name="insertislemi">CREATE ACCOUNT</button> <br> <br>
+					
 
 
-			</form>
+				</form>
 
+
+			</div>
 
 		</div>
-
-	</div>
-
-</body>
-</html>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+	</body>
+	</html>
