00bf1c47946c864a5f34b233828ebfe7dce45abf
gizemmuratoglu <58625642+gizemmuratoglu@users.noreply.github.com>
2021-01-30 18:15:32 +0300

    Update userbudget.php

--- userbudget.php
+++ userbudget.php
@@ -1,11 +1,16 @@
 <?php
 include 'baglan.php';
+session_start();
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>HostDetail/ERENHOUSES</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<style >
 		@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300&display=swap');
 		body{
@@ -34,7 +39,7 @@ include 'baglan.php';
 </head>
 <body>
 	<nav class="navbar navbar-dark bg-dark navbar-expand-lg text-white"> 
-		<div class="container py-2"> <a  class="navbar-brand"> ERENHOUSES </a>
+		<div class="container py-2"> <a  class="navbar-brand"> Welcome <?php echo $_SESSION['name'];  ?> </a>
 
 			<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-houses" aria-controls="navbar-houses" >
 				<span class="navbar-toggler-icon"> </span>
@@ -54,6 +59,9 @@ include 'baglan.php';
 					<li class="nav-item px-4">
 						<a href="reportPage.php?id=<?php echo $_GET['id']; ?>" class="nav-link text-white ">REPORT PROBLEM&REQUEST</a>
 					</li>
+					<li class="nav-item px-4 ">
+							<a href="out.php" class="nav-link text-white ">LOG OUT</a>
+						</li>
 					
 				</ul>
 			</div>
@@ -63,7 +71,7 @@ include 'baglan.php';
 
 	<br><br>
 	<div class="container">
-		<table style=" width: 100%;" border="1px" class="table table-secondary table-striped table-bordered table-hover">
+		<table style=" width: 82%; margin-left: 115px;" border="1px" class="table table-secondary table-striped table-bordered table-hover">
 			<tr>
 
 				<th >TERM</th>
@@ -80,7 +88,7 @@ include 'baglan.php';
 
 
 			<?php
-			$bilgilerisor=$db->prepare("SELECT * FROM gelirgider ORDER BY tim ");
+			$bilgilerisor=$db->prepare("SELECT * FROM gelirgider ORDER BY tim DESC");
 			$bilgilerisor->execute();
 			$sayı=0;
 			while ($bilgileriçek=$bilgilerisor->FETCH(PDO::FETCH_ASSOC)) { $sayı++; ?>
@@ -90,12 +98,12 @@ include 'baglan.php';
 
 				<tr>
 					
-					<td><?php echo $bilgileriçek['donem'] ; ?></td>
-					<td style="text-align: center;"><?php echo $bilgileriçek['elektrik'] ; ?></td>
-					<td style="text-align: center;"><?php echo $bilgileriçek['su'] ; ?></td>
-					<td style="text-align: center;"><?php echo $bilgileriçek['temizlik'] ; ?></td>
-					<td style="text-align: center;"><?php echo $bilgileriçek['diger'] ; ?></td>
-					<td style="text-align: center;"><?php echo $bilgileriçek['tim'] ; ?></td>
+					<td style="font-size: 17px;"><?php echo $bilgileriçek['donem'] ; ?></td>
+					<td style="text-align: center; font-size: 17px;"><?php echo $bilgileriçek['elektrik'] ; ?></td>
+					<td style="text-align: center; font-size: 17px;"><?php echo $bilgileriçek['su'] ; ?></td>
+					<td style="text-align: center; font-size: 17px;"><?php echo $bilgileriçek['temizlik'] ; ?></td>
+					<td style="text-align: center; font-size: 17px;"><?php echo $bilgileriçek['diger'] ; ?></td>
+					<td style="text-align: center; font-size: 17px;"><?php echo $bilgileriçek['tim'] ; ?></td>
 					
 					
 				</tr>
